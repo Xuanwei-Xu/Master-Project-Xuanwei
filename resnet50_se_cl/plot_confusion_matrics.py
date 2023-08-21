bd0fb0ee871047ef2df9e213c60e86a93496bbdb
Xuanwei-Xu <142600249+Xuanwei-Xu@users.noreply.github.com>
2023-08-21 18:13:22 +0100

    plot_confusion_matrics.py

--- resnet50_se_cl/plot_confusion_matrics.py
+++ resnet50_se_cl/plot_confusion_matrics.py
@@ -19,15 +19,10 @@ import matplotlib.pyplot as plt
 os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 
 
-# 此文件注释可参考 train_resnet.py
 
 
 class ConfusionMatrix(object):
-    """
-    注意，如果显示的图像不全，是matplotlib版本问题
-    本例程使用matplotlib-3.2.1(windows and ubuntu)绘制正常
-    需要额外安装prettytable库
-    """
+    
 
     def __init__(self, num_classes: int, labels: list):
         self.matrix = np.zeros((num_classes, num_classes))
@@ -66,21 +61,17 @@ class ConfusionMatrix(object):
         print(matrix)
         plt.imshow(matrix, cmap=plt.cm.Blues)
 
-        # 设置x轴坐标label
         plt.xticks(range(self.num_classes), self.labels, rotation=45)
-        # 设置y轴坐标label
         plt.yticks(range(self.num_classes), self.labels)
-        # 显示colorbar
+
         plt.colorbar()
         plt.xlabel('True Labels')
         plt.ylabel('Predicted Labels')
         plt.title('Confusion matrix')
 
-        # 在图中标注数量/概率信息
         thresh = matrix.max() / 2
         for x in range(self.num_classes):
             for y in range(self.num_classes):
-                # 注意这里的matrix[y, x]不是matrix[x, y]
                 info = int(matrix[y, x])
                 plt.text(x, y, info,
                          verticalalignment='center',
@@ -159,7 +150,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_classes', type=int, default=4)
     parser.add_argument('--batch-size', type=int, default=32)
     parser.add_argument('--data-path', type=str,
-                        default="../brain_dataset_24b")  ###################验证集单独路径，和元数据集格式相同！
+                        default="../brain_dataset_24b") 
     parser.add_argument('--freeze-layers', type=bool, default=False)
     parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
     opt = parser.parse_args()
